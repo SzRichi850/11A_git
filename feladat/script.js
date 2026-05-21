cc4c529228cde244c27ad5b67c9f2559626fc654
Xavér <kompleter540@gmail.com>
2026-05-21 11:53:45 +0200

    update_html

--- feladat/script.js
+++ feladat/script.js
@@ -1,7 +1,23 @@
-const doneTask = document.querySelector(".done");
+const doneTask_1 = document.querySelector(".shop_done");
 
-doneTask.addEventListener("click", function() {
-    doneTask.textContent = "Kész";
-    doneTask.style.backgroundColor = "pink";
-    doneTask.style.color = "black";
+doneTask_1.addEventListener("click", function() {
+    doneTask_1.textContent = "Kész";
+    doneTask_1.style.backgroundColor = "pink";
+    doneTask_1.style.color = "black";
+});
+
+const doneTask_2 = document.querySelector(".learn_done");
+
+doneTask_2.addEventListener("click", function() {
+    doneTask_2.textContent = "Kész";
+    doneTask_2.style.backgroundColor = "pink";
+    doneTask_2.style.color = "black";
+});
+
+const doneTask_3 = document.querySelector(".clean_done");
+
+doneTask_3.addEventListener("click", function() {
+    doneTask_3.textContent = "Kész";
+    doneTask_3.style.backgroundColor = "pink";
+    doneTask_3.style.color = "black";
 });
