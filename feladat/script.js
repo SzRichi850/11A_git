b66e705b999c7cca53d8b24b75416a425edc007b
Lackhoee <ifjjuhaszlaszlo2008@gmail.com>
2026-05-21 19:51:14 +0200

    reszponziv css
    A css megcsinalva

---
+++ feladat/script.js
@@ -0,0 +1,21 @@
+<<<<<<< Updated upstream
+=======
+let gombok = document.getElementsByClassName("kesz-btn");
+
+for (let i = 0; i < gombok.length; i++) {
+    gombok[i].onclick = function(esemeny) {
+        let gomb = esemeny.target;
+        let gombTarolo = gomb.parentElement;
+        let listaElem = gombTarolo.parentElement;
+        let szoveg = listaElem.querySelector(".feladat-szoveg");
+
+        szoveg.style.backgroundColor = "transparent";
+        szoveg.style.textDecoration = "line-through";
+        szoveg.style.opacity = "50%";
+        gomb.remove();
+
+        let keszLista = document.getElementById("kesz-lista");
+        keszLista.appendChild(listaElem);
+    }
+}
+>>>>>>> Stashed changes
